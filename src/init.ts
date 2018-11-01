568421d091e6d395ae88991c140c5eca98aae9d7
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-11-01 11:59:42 +0100

    Removes Elm

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,3 @@
 import { init } from './main'
 const canvasId = 'canvas'
-import('./Main.elm').then(({ Elm }) => Elm.Main.init({
-  node: document.getElementById('app'),
-  flags: {
-    canvasId
-  }
-})
-).then(_ => init(canvasId))
+init(canvasId)
